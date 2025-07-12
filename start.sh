5a3a2f825b26873c09ff3c9ced049b79256c503e
Ravishankar Singh <rr9110029069@gmail.com>
2025-07-12 22:23:00 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export HOME=/tmp
+export OLLAMA_MODELS=/tmp/.ollama
+
+ollama serve &
+sleep 5
+ollama pull tinyllama
+
+
+# Run the app from src/
+streamlit run src/app.py --server.port=7860 --server.address=0.0.0.0
+
